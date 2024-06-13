31a8b5520e984669caca28c904476fa4db62406d
yannxiaoyanhu <129284444+yannxiaoyanhu@users.noreply.github.com>
2024-06-13 11:08:50 +0800

    Update get_models.py

--- gen_models/get_models.py
+++ gen_models/get_models.py
@@ -7,8 +7,8 @@ import torch.nn.functional as F
 def get_imagenet_models(device, img_size=128, num_classes=1000):
     """
     Load pretrained generative models of the ImageNet dataset. The pretrained weights are downloaded from
-    the official repository of Studio GAN: https://github.com/POSTECH-CVLab/PyTorch-StudioGAN, which are
-    saved under the directory ./gen_models/pretrained_weights.
+    the official repository of Studio GAN: https://github.com/POSTECH-CVLab/PyTorch-StudioGAN and its hugging
+    face page: https://huggingface.co/Mingguksky/PyTorch-StudioGAN.
     """
     magnitude_ema_beta = 0.5 ** (256 * 1 / (20 * 1e3))
     styleGAN3 = stylegan3.Generator(z_dim=512, c_dim=1000, w_dim=512, img_resolution=128, img_channels=3,
@@ -18,6 +18,7 @@ def get_imagenet_models(device, img_size=128, num_classes=1000):
                                                       "num_fp16_res": 0, "conv_clamp": None, "conv_kernel": 3,
                                                       "use_radial_filters": False,
                                                       "magnitude_ema_beta": magnitude_ema_beta}).to(device)
+    # Download from https://huggingface.co/Mingguksky/PyTorch-StudioGAN/blob/main/studiogan_official_ckpt/ImageNet_tailored/ImageNet-StyleGAN3-t-SPD-train-2022_02_16_12_40_58/model%3DG_ema-best-weights-step%3D94000.pth
     ckpt_path_stylegan3 = './pretrained_weights/stylegan3=G_ema-best-weights-step=94000.pth'
     ckpt_stylegan3 = torch.load(ckpt_path_stylegan3, map_location=lambda storage, loc: storage)
     styleGAN3.load_state_dict(ckpt_stylegan3["state_dict"], strict=True)
@@ -26,6 +27,8 @@ def get_imagenet_models(device, img_size=128, num_classes=1000):
     SNGAN = resnet.Generator(z_dim=128, g_shared_dim='N/A', img_size=128, g_conv_dim=64, apply_attn=False,
                              attn_g_loc=['N/A'], g_cond_mtd='cBN', num_classes=1000,
                              g_init='ortho', g_depth='N/A', mixed_precision=False).to(device)
+
+    # Download from https://huggingface.co/Mingguksky/PyTorch-StudioGAN/blob/main/studiogan_official_ckpt/ImageNet_tailored/ImageNet-SNGAN-256-train-2022_03_06_03_53_44/model%3DG-best-weights-step%3D472000.pth
     ckpt_path_SNGAN = './pretrained_weights/SNGAN=G-best-weights-step=472000.pth'
     ckpt_SNGAN = torch.load(ckpt_path_SNGAN, map_location=lambda storage, loc: storage)
     SNGAN.load_state_dict(ckpt_SNGAN["state_dict"], strict=True)
@@ -35,6 +38,7 @@ def get_imagenet_models(device, img_size=128, num_classes=1000):
                                   attn_g_loc=[4], g_cond_mtd="cBN", num_classes=num_classes, g_init="ortho",
                                   g_depth="N/A",
                                   mixed_precision=False).to(device)
+    # Download from https://drive.google.com/drive/folders/1_RTYZ0RXbVLWufE7bbWPvp8n_QJbA8K0?usp=sharing
     ckpt_path_BigGAN = './pretrained_weights/BigGAN=G_ema-best-weights-step=198000.pth'
     ckpt_BigGAN = torch.load(ckpt_path_BigGAN, map_location=lambda storage, loc: storage)
     BigGAN.load_state_dict(ckpt_BigGAN["state_dict"], strict=True)
@@ -43,11 +47,13 @@ def get_imagenet_models(device, img_size=128, num_classes=1000):
     ContraGAN = big_resnet.Generator(z_dim=120, g_shared_dim=128, img_size=img_size, g_conv_dim=96, apply_attn=True,
                                      attn_g_loc=[4], g_cond_mtd="cBN", num_classes=num_classes, g_init="ortho",
                                      g_depth="N/A", mixed_precision=False).to(device)
+    # Download from https://drive.google.com/drive/folders/1pbP6LQ00VF7si-LXLvd_D00Pk5_E_JnP?usp=sharing
     ckpt_path_ContraGAN = './pretrained_weights/ContraGAN=G_ema-best-weights-step=198000.pth'
     ckpt_ContraGAN = torch.load(ckpt_path_ContraGAN, map_location=lambda storage, loc: storage)
     ContraGAN.load_state_dict(ckpt_ContraGAN["state_dict"], strict=True)
     print('Successfully loading trained ContraGAN!')
 
+    # Download from https://drive.google.com/drive/folders/1lWw6Oh_Mjc7BKiSUKhWxfgP9QLc45g8a?usp=sharing
     ReACGAN = big_resnet.Generator(z_dim=120, g_shared_dim=128, img_size=img_size, g_conv_dim=96, apply_attn=True,
                                    attn_g_loc=[4], g_cond_mtd="cBN", num_classes=num_classes, g_init="ortho",
                                    g_depth="N/A", mixed_precision=False).to(device)
